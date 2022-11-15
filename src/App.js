c778d184065add06acfd31010da6d725cb842b38
DEMETRIUS CARNEIRO MONTEIRO FERREIRA <68928535+demithehomie@users.noreply.github.com>
2022-11-15 11:15:03 -0300

    terceiro deploy

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-import imagem from "../src/um.svg";
-import simone from "../src/imgs/simone.jpeg"
+import imagem from "../src/imgs/um.jpg";
+import simone from "../src/imgs/simone.jpeg";
+import antonia from "../src/imgs/antonia.jpeg";
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 /*
-
 
       <div className="navbar">
         <h5 className="navbar-items">O que é?</h5>
@@ -16,18 +17,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="app">
-     
+      <div className="container-fluid">
+      <div className="box2">
+        <h2 className="header-text">O que o seu espelho te mostra te agrada?</h2>        
+  </div>
 
-    <div className="container-fluid">
-    <div className="box2">
-  <h2>O que o seu espelho tem te mostrado te agrada?</h2>
-          
-</div>
       <div className="header">
       <br/>
         <h1 className="xxl-text-title"><strong>Você conhece o Mais Vida?</strong></h1>
         <br/>
-        <h3 className="subtitle1">Receitas 100% naturais, por uma vida mais saudável</h3>
+        <h3 className="subtitle1">Receitas 100% naturais para o seu bem-estar</h3>
         <br/>
         <h4 className="subtitle2">Você vai amar a praticidade desse produto! </h4> 
       <br/>
@@ -50,6 +49,12 @@ function App() {
       <br/>
       <br/>
       <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+
       
       <br/>
       <br/>
@@ -63,19 +68,20 @@ function App() {
         <h2 className="navbar-items-desc">O que é?</h2>
         <h5 className="navbar-items-desc-det">
           <p>
-          Um ebook preparado por especialistas para te ajudar em todas as etapas do seu emagrecimento. Um guia para o emagrecimento na ponta dos seus dedos.</p></h5>
+          Este e-book é seu, e foi criado pensando em você e sua família, para que haja mais qualidade de vida e bem-estar. Com um estilo de vida equilibrado você terá uma vida melhor e feliz.
+          </p></h5>
     </div>   
 
     <div>
         <h2 className="navbar-items-desc">Como funciona?</h2>
         <h5 className="navbar-items-desc-det">
-          Por ser um produto digital, você pode consumir ele como quiser, pode ler no seu telefone, computador, televsão ou até imprimir ele se quiser. Confortável e maleável.
+        Por ser um produto digital, você pode levar para onde quiser. A leitura pode ser feita pelo seu celular, computador, TV ou até mesmo, se desejar, imprimir. Conforto e praticidade é a proposta, para que a sua vida seja melhor.
         </h5>
     </div> 
         <div>
           <h2 className="navbar-items-desc">Como adquirir?</h2>
             <h5 className="navbar-items-desc-det">
-          Você pode baixar no seu computador ou celular e ler tranquilamente. Oferecemos atualizações gratuitas, então você não precisa se preocupar.
+            A vida é aqui e agora, por isso não dá pra deixar pra depois. Comece já a adotar um Estilo de Vida Saudável com essas deliciosas dicas de sucos e shakes 100% naturais. Em breve novas receitas.
           </h5>
 
         </div>
@@ -91,10 +97,11 @@ function App() {
       </div>
       <br/>
 
-        <h2 className="navbar-items-desc">Confira agora alguns cases de quem já desfrutou do ebook</h2>
-
-        <img className="img-depo1" src={simone} alt="depoimento das mulheres"/>
-
+        <h2 className="navbar-items-desc">Confira agora alguns cases de quem já desfrutou do e-book</h2>
+        <div className="depoimentos-imgs">
+          <img className="img-depo1" src={simone} alt="depoimento das mulheres"/>
+          <img className="img-depo1" src={antonia} alt="depoimento da antonia"/>
+        </div>
         <div className="box1">
           
           <h3>
@@ -106,11 +113,29 @@ Sabe por que? Porque você vai amar o que verá no seu espelho!</h3>
 
 <br/>
 
-<h2 className="navbar-items-desc-t">Quem sou eu?</h2>
+<h2 className="navbar-items-desc-t">E aí, beleza? </h2>
 <div className="navbar-items-desc-quem">
   
  
-  <h4 className="navbar-items-desc-det">E aí, beleza? Eu sou Willian Balduino e nos últimos 10 anos, antes da pandemia, trabalhei levando conteúdos para melhoria da qualidade de vida das pessoas em empresas, escolas, residências, tanto em palestras como em visitas. Os conteúdos sempre foram direcionados para o relacionamento, educação e cuidados com a saúde, portanto, voltado única e exclusivamente para o equilíbrio e busca de uma qualidade de vida melhor. Este conteúdo que está sendo disponibilizado aqui para você é para mudar a sua vida do antes e depois, Aproveite bem e tenha Mais Vida!</h4>
+  <h4 className="navbar-items-desc-det">Sou e Willian Balduino atuei nos últimos 10 anos levando conteúdos e informações para melhoria da qualidade de vida das pessoas em empresas, escolas, residências, tanto em palestras como em visitas. 
+Os conteúdos sempre foram direcionados para o relacionamento, educação e bem-estar, portanto, voltado única e exclusivamente para o equilíbrio e busca de uma qualidade de vida melhor.
+A pandemia mudou o meu cenário, não o meu propósito, por isso desejo que você tenha Mais Vida com este conteúdo, que está disponível aqui. Aproveite bem e tenha Mais Vida!</h4>
+</div>
+
+<div>
+  <br/>
+  <h3  className="navbar-items-desc-t">
+    Comprando nos próximos 15 min você receberá uma surpresa especial.
+  </h3>
+</div>
+
+<div>
+  <br/>
+  <h2 className="navbar-items-desc-t">
+ <strong>BÔNUS ESPECIAL</strong> </h2>
+  <h3 className="navbar-items-desc-det">
+Você tomou a decisão que vai mudar a sua vida para melhor, e para tornar os seus hábitos ainda melhores, receba este bônus super especial das receitas de leite vegetal, para enriquecer as suas receitas e o seu dia a dia.
+</h3>
 </div>
     
 
@@ -124,6 +149,7 @@ Sabe por que? Porque você vai amar o que verá no seu espelho!</h3>
 <br/>
  <div>
         <button className="botao-de-compra" onClick={() => window.open("https://pay.kiwify.com.br/zD2Vx51")}>Comprar agora</button>
+        
         <h6 className="subtitle3">Receba o seu produto instantaneamente</h6>
 
       </div>
@@ -138,7 +164,7 @@ Sabe por que? Porque você vai amar o que verá no seu espelho!</h3>
           Você está feliz com o seu estilo de vida hoje? 
           Olhe no espelho agora e imagine-se no futuro. 
 
-Gostou? Então comece já a executar as receitas para ter Mais Vida e bem-estar.</h3>
+            Gostou? Então comece já a executar as receitas para ter Mais Vida e bem-estar.</h3>
         </div>
 
 
@@ -151,9 +177,11 @@ Gostou? Então comece já a executar as receitas para ter Mais Vida e bem-estar.
     <br/>
       <h2 className="navbar-items-desc">Alguma dúvida? Fale conosco agora mesmo</h2>
     <br/>
- 
-    <h4 className="botao-de-compra" onClick={() => window.open(" https://linkwhats.app/8ae376")}>Clique aqui e converse com o autor via WhatsApp</h4>
-
+ <div className="botoes">
+    <h4 className="botao-de-compra-2" onClick={() => window.open(" https://linkwhats.app/8ae376")}>WhatsApp</h4>
+    <h4 className="botao-de-compra-2" onClick={() => window.open(" https://www.instagram.com/balduino_willian2208")}>Instagram</h4>
+    <h4 className="botao-de-compra-2" onClick={() => window.open("mailto:william.balduino@gmail.com?subject=Ajuda%20sobre%20o%20produto%20Mais%20Vida&body=Ol%C3%A1%2C%20pode%20me%20ajudar%3F")}>Email</h4>
+</div>
     <h6 className="footer-message">Makron Agência Digital (C) 2022. Todos os Direitos Reservados</h6>
     </div>
   );
