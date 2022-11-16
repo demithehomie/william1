df76ea4a2330cccf81ee4243984172deda44571d
DEMETRIUS CARNEIRO MONTEIRO FERREIRA <68928535+demithehomie@users.noreply.github.com>
2022-11-16 19:26:01 -0300

    sétimo_deploy

--- src/App.js
+++ src/App.js
@@ -31,9 +31,14 @@ function App() {
         <h3 className="subtitle1">Receitas 100% naturais para o seu bem-estar</h3>
         <br/>
         <h4 className="subtitle2">Você vai amar a praticidade desse produto! </h4> 
-      <br/>
-      <br/>
+        <br/>
+        <br/>
       
+      <iframe className="video_yt" width="560" height="315" src="https://www.youtube.com/embed/W0EaKB18PY8?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <br/>
+      <br/><br/>
+      <br/>
+
       <div className="newbox">
         
         <img className="img-book" onClick={() => window.open("https://pay.kiwify.com.br/zD2Vx51")} src={imagem} alt="ebook"/>
@@ -51,7 +56,20 @@ function App() {
       <br/>
       <br/>
       <br/>
-   
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
      
       <br/>
 
@@ -119,10 +137,14 @@ Sabe por que? Porque você vai amar o que verá no seu espelho!</h3>
 <div className="navbar-items-desc-quem">
   
  
-  <h4 className="navbar-items-desc-det-2">Sou eu Willian Balduino atuei nos últimos 10 anos levando conteúdos e informações para melhoria da qualidade de vida das pessoas em empresas, escolas, residências, tanto em palestras como em visitas. 
-Os conteúdos sempre foram direcionados para o relacionamento, educação e bem-estar, portanto, voltado única e exclusivamente para o equilíbrio e busca de uma qualidade de vida melhor.
-A pandemia mudou o meu cenário, não o meu propósito, por isso desejo que você tenha Mais Vida com este conteúdo, que está disponível aqui. Aproveite bem e tenha Mais Vida!</h4>
+  <h4 className="navbar-items-desc-det-2">
+Sou Willian balduino e atuei nos últimos 10 anos levando conteúdos e informações  voltados para a melhoria da qualidade de vida das pessoas. 
+<br/><br/>
+Esta atuação foi realizada como palestrante em empresas, escolas, igrejas e também em visitas familiares. 
+<br/><br/>Os conteúdos sempre foram direcionados para a melhoria nos relacionamentos, educação, cidadania, a busca pelo equilíbrio físico, mental, social e espiritual, enfim, o bem-estar familiar e corporativo.<br/><br/> A pandemia mudou o meu cenário e rota, mas não o meu propósito, por isso desejo que você e sua família tenha Mais Vida com este conteúdo. <br/><br/>Aproveite bem fazendo uso diário e tenha Mais Vida hoje e em todos os dias à partir de agora!</h4>
 </div>
+
+
 
 <div>
   <br/>
@@ -188,8 +210,8 @@ Você tomou a decisão que vai mudar a sua vida para melhor, e para tornar os se
       <h2 className="navbar-items-desc">Fale conosco agora mesmo</h2>
     <br/>
  <div className="botoes">
-    <h4 className="botao-de-compra-2" onClick={() => window.open(" https://linkwhats.app/8ae376")}>WhatsApp</h4>
-    <h4 className="botao-de-compra-2" onClick={() => window.open(" https://www.instagram.com/balduino_willian2208")}>Instagram</h4>
+    <h4 className="botao-de-compra-2" onClick={() => window.open("https://linkwhats.app/8ae376")}>WhatsApp</h4>
+    <h4 className="botao-de-compra-2" onClick={() => window.open("https://www.instagram.com/balduino_willian2208")}>Instagram</h4>
     <h4 className="botao-de-compra-2" onClick={() => window.open("mailto:william.balduino@gmail.com?subject=Ajuda%20sobre%20o%20produto%20Mais%20Vida&body=Ol%C3%A1%2C%20pode%20me%20ajudar%3F")}>Email</h4>
 </div>
     <h6 className="footer-message">Makron Agência Digital (C) 2022. Todos os Direitos Reservados</h6>
