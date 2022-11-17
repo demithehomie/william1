9e582ccf11abfd0114039b80356c49a9e48f3d47
DEMETRIUS CARNEIRO MONTEIRO FERREIRA <68928535+demithehomie@users.noreply.github.com>
2022-11-16 23:42:35 -0300

    oitavo_deploy

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-import imagem from "../src/imgs/um.jpg";
+
 import simone from "../src/imgs/simone.jpeg";
 import antonia from "../src/imgs/antonia.jpeg";
 import dani_bau from "../src/imgs/dani_bau.jpeg";
 import diana from "../src/imgs/diana.jpeg";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 /*
 
       <div className="navbar">
@@ -20,90 +21,64 @@ function App() {
   return (
     <div className="app">
       <div className="container-fluid">
-      <div className="box2">
-        <h2 className="header-text">O que o seu espelho te mostra te agrada?</h2>        
-  </div>
-
-      <div className="header">
-      <br/>
-        <h1 className="xxl-text-title"><strong>Você conhece o Mais Vida?</strong></h1>
-        <br/>
-        <h3 className="subtitle1">Receitas 100% naturais para o seu bem-estar</h3>
-        <br/>
-        <h4 className="subtitle2">Você vai amar a praticidade desse produto! </h4> 
-        <br/>
-        <br/>
+        <div className="header">
+           <br/>
+              <h1 className="xxl-text-title"><strong>Você conhece o Mais Vida?</strong></h1>
+            <br/>
+              <h3 className="subtitle1">Receitas 100% naturais para o seu bem-estar</h3>
+            <br/>
+              <h4 className="subtitle2">Você vai amar a praticidade desse produto! </h4> 
+            <br/>
+            <br/>
       
-      <iframe className="video_yt" width="560" height="315" src="https://www.youtube.com/embed/W0EaKB18PY8?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe className="video_yt" src="https://www.youtube.com/embed/W0EaKB18PY8?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+   <br/>
       <br/>
-      <br/><br/>
       <br/>
 
-      <div className="newbox">
-        
-        <img className="img-book" onClick={() => window.open("https://pay.kiwify.com.br/zD2Vx51")} src={imagem} alt="ebook"/>
-       
-      </div>
-      <br/>
-      <br/>
-        </div>
-
-        <div>
-        <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-     
-      <br/>
-
-<br/>
-      <br/>
-      
-      
-      <br/>
-      <br/>
-        <button className="botao-de-compra" onClick={() => window.open("https://pay.kiwify.com.br/zD2Vx51")}>Comprar agora</button>
+      <button className="botao-de-compra" onClick={() => window.open("https://pay.kiwify.com.br/zD2Vx51")}>Comprar agora</button>
         <h6 className="subtitle3">Receba o seu produto instantaneamente</h6>
-      </div>
+        
       <br/>
-
-    <div>
-    </div>
-        <h2 className="navbar-items-desc">O que é?</h2>
+      <br/>
+      
+     
+      <h2 className="navbar-items-desc">O que é?</h2>
         <h5 className="navbar-items-desc-det">
           <p>
           Este e-book é seu, e foi criado pensando em você e sua família, para que haja mais qualidade de vida e bem-estar. Com um estilo de vida equilibrado você terá uma vida melhor e feliz.
-          </p></h5>
-    </div>   
+          </p>
+          </h5>
+
+          <h2 className="navbar-items-desc">Como funciona?</h2>
+        <h5 className="navbar-items-desc-det">
+        Por ser um produto digital, você pode levar para onde quiser. A leitura pode ser feita por celular, computador, TV ou até mesmo, se desejar, imprimir. Conforto e praticidade é a proposta, para que a sua vida seja melhor.
+        </h5>
+
+        <br/>
+
+        <h2 className="navbar-items-desc">Como adquirir?</h2>
+            <h5 className="navbar-items-desc-det">
+            A vida é aqui e agora, por isso não dá pra ficar adiando, comece já a mudar seus hábitos para um Estilo de Vida Saudável com essas deliciosas dicas de sucos e shakes 100% naturais. 
+            <br/>
+            <br/>
+            <br/>
+            <strong>Em breve novas receitas.</strong>
+          </h5>
+          
+      
+      
+      <br/>
+
+    
+      
+       
 
     <div>
-        <h2 className="navbar-items-desc">Como funciona?</h2>
-        <h5 className="navbar-items-desc-det">
-        Por ser um produto digital, você pode levar para onde quiser. A leitura pode ser feita pelo seu celular, computador, TV ou até mesmo, se desejar, imprimir. Conforto e praticidade é a proposta, para que a sua vida seja melhor.
-        </h5>
+     
     </div> 
         <div>
-          <h2 className="navbar-items-desc">Como adquirir?</h2>
-            <h5 className="navbar-items-desc-det">
-            A vida é aqui e agora, por isso não dá pra ficar adiando, por isso, comece já a mudar seus hábitos para um Estilo de Vida Saudável com essas deliciosas dicas de sucos e shakes 100% naturais. <strong>Em breve novas receitas.</strong>
-          </h5>
+      
 
         </div>
     
@@ -124,9 +99,9 @@ function App() {
         </div>
         <div className="box1">
           
-          <h3>
+          <h4 className="navbar-items-desc-det-2">
 O seu bem-estar e qualidade de vida depende da sua atitude positiva em começar. Faça uso diariamente das receitas deste material, que foi especialmente feito para você!
-Sabe por que? Porque você vai amar o que verá no seu espelho!</h3>
+</h4>
         </div>
 
        
@@ -138,10 +113,22 @@ Sabe por que? Porque você vai amar o que verá no seu espelho!</h3>
   
  
   <h4 className="navbar-items-desc-det-2">
-Sou Willian balduino e atuei nos últimos 10 anos levando conteúdos e informações  voltados para a melhoria da qualidade de vida das pessoas. 
-<br/><br/>
+      Sou Willian balduino e atuei nos últimos 10 anos levando conteúdos e informações voltados para a melhoria da qualidade de vida das pessoas. 
+    <br/>
+    <br/>
 Esta atuação foi realizada como palestrante em empresas, escolas, igrejas e também em visitas familiares. 
-<br/><br/>Os conteúdos sempre foram direcionados para a melhoria nos relacionamentos, educação, cidadania, a busca pelo equilíbrio físico, mental, social e espiritual, enfim, o bem-estar familiar e corporativo.<br/><br/> A pandemia mudou o meu cenário e rota, mas não o meu propósito, por isso desejo que você e sua família tenha Mais Vida com este conteúdo. <br/><br/>Aproveite bem fazendo uso diário e tenha Mais Vida hoje e em todos os dias à partir de agora!</h4>
+    <br/>
+    <br/>
+Os conteúdos sempre foram direcionados para a melhoria nos relacionamentos, educação, cidadania, a busca pelo equilíbrio físico, mental, social e espiritual, enfim, o bem-estar familiar e corporativo.
+
+    <br/>
+    <br/>
+ A pandemia mudou o meu cenário e rota, mas não o meu propósito, por isso desejo que você e sua família tenha Mais Vida com este conteúdo. 
+    
+    <br/>    
+    <br/>
+
+Aproveite bem fazendo uso diário e tenha Mais Vida hoje e em todos os dias à partir de agora!</h4>
 </div>
 
 
@@ -149,7 +136,7 @@ Esta atuação foi realizada como palestrante em empresas, escolas, igrejas e ta
 <div>
   <br/>
   <h3  className="navbar-items-desc-t">
-    Comprando nos próximos 15 min você receberá uma surpresa especial.
+    Comprando nos próximos 15 minutos você receberá uma surpresa especial.
   </h3>
 </div>
 
@@ -190,12 +177,7 @@ Você tomou a decisão que vai mudar a sua vida para melhor, e para tornar os se
 <br/>         <br/>
 
  Agora é a hora de você usar mais o seu liquificador e a centrífuga, vá para a cozinha e mude seus hábitos, para colher melhores resultados hoje, amanhã e depois, depois...
-          <br/><br/>
-          Você está feliz com o seu estilo de vida hoje? 
-          <br/>
-          Olhe no espelho agora e imagine-se no futuro. 
-          <br/>
-            Gostou? Então comece já a executar as receitas para ter Mais Vida e bem-estar.</h3>
+</h3>
         </div>
 
 
@@ -215,6 +197,8 @@ Você tomou a decisão que vai mudar a sua vida para melhor, e para tornar os se
     <h4 className="botao-de-compra-2" onClick={() => window.open("mailto:william.balduino@gmail.com?subject=Ajuda%20sobre%20o%20produto%20Mais%20Vida&body=Ol%C3%A1%2C%20pode%20me%20ajudar%3F")}>Email</h4>
 </div>
     <h6 className="footer-message">Makron Agência Digital (C) 2022. Todos os Direitos Reservados</h6>
+    </div>
+    </div>
     </div>
   );
 }
